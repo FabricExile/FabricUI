6fb86e536dbf7bd31acb644ff7508c8824e4a070
Julien Couet <couet.julien@gmail.com>
2017-08-16 20:44:21 -0400

    Wrapp FabricUI::DFG::DFGPVToolActions in python

--- shiboken/global_dfg.h
+++ shiboken/global_dfg.h
@@ -5,3 +5,5 @@
 #include <FabricUI/DFG/Commands/DFGCommandRegistration.h>
 #include <FabricUI/DFG/Tools/DFGToolsCommandRegistration.h>
 #include <FabricUI/DFG/Tools/DFGPVToolsNotifierRegistry.h>
+#include <FabricUI/DFG/Tools/DFGPVToolActions.h>
+#include <FabricUI/DFG/Tools/DFGVEEditorPVToolActions.h>
