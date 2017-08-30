2ac2e98ae003ec66e9bf4e1930cd97880dc883f8
Helge Mathee <helge.mathee@fabricengine.com>
2017-08-30 17:55:03 +0200

    osx build breaks for maya 2018

--- DFG/DFGExecNotifier.h
+++ DFG/DFGExecNotifier.h
@@ -12,7 +12,7 @@
 #include <FTL/ArrayRef.h>
 #include <FTL/StrRef.h>
 #include <QSharedPointer>
-#if defined(FTL_PLATFORM_WINDOWS)
+#if defined(FTL_PLATFORM_WINDOWS) or defined(FABRICUI_OSX_CPP11)
 # include <unordered_map>
 #else
 # include <tr1/unordered_map>
@@ -517,7 +517,7 @@ private:
 
   FabricCore::DFGView m_view;
 
-#if defined(FTL_PLATFORM_WINDOWS)
+#if defined(FTL_PLATFORM_WINDOWS) or defined(FABRICUI_OSX_CPP11)
   typedef std::unordered_map<
 #else
   typedef std::tr1::unordered_map<
