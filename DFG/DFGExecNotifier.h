66b320503dc69276a3aee26dfc0af4a40f17ae48
Helge Mathee <helge.mathee@fabricengine.com>
2017-09-26 14:22:20 +0200

    FE-8825: revert fix for osx

--- DFG/DFGExecNotifier.h
+++ DFG/DFGExecNotifier.h
@@ -12,9 +12,9 @@
 #include <FTL/ArrayRef.h>
 #include <FTL/StrRef.h>
 #include <QSharedPointer>
-#if defined(FTL_OS_WINDOWS)
+#if defined(FTL_PLATFORM_WINDOWS)
 # include <unordered_map>
-#elif defined(FTL_OS_DARWIN)
+#elif defined(FABRICUI_OSX_CPP11)
 # include <unordered_map>
 #else
 # include <tr1/unordered_map>
@@ -519,9 +519,9 @@ private:
 
   FabricCore::DFGView m_view;
 
-#if defined(FTL_OS_WINDOWS)
+#if defined(FTL_PLATFORM_WINDOWS)
   typedef std::unordered_map<
-#elif defined(FTL_OS_DARWIN)
+#elif defined(FABRICUI_OSX_CPP11)
   typedef std::unordered_map<
 #else
   typedef std::tr1::unordered_map<
